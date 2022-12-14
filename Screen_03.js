f3dcd0770eadfc163722d01d17b903d1eff4572e
phongok <phongdevgithub666666@gmail.com>
2022-12-14 13:56:04 +0700

    update screen3

--- Screen_03.js
+++ Screen_03.js
@@ -2,17 +2,24 @@ import { StyleSheet, View, Image, Text, Button } from 'react-native';
 export default function Screen_03 ({route}) {
     return (
         <View style={styles.container}>
-            <View>
-                <Image source={{uri: route.params.productsNew[0].url}} style={{ width: '40%', height: '40%', marginLeft: '30%', marginTop: '20%' }}></Image>
+            <View style={{flex:1}} >
+                <Image source={{uri: route.params.productsNew[0].url}} style={{ width: '50%', height: '50%', marginLeft: '20%', marginTop: '20%' }}></Image>
             </View>
-           <View style={styles.conten1}>
-              
-              <Text style={{fontSize:20, marginTop:20, marginLeft:15, fontWeight:'bold'}}>{route.params.productsNew[0].name}</Text>
+           <View style={styles.conten1}>  
+              <Text style={{fontSize:20, marginLeft:15, fontWeight:'bold'}}>{route.params.productsNew[0].name}</Text>
               <View style={{flexDirection:'row', marginTop:10}}>
               <Text style={{textAlign: 'center', fontSize:20, marginLeft:30, marginTop:10}}>15% OFF </Text>
               <Text style={{textAlign: 'center', fontSize:20,marginLeft:30, marginTop:10}}>{route.params.productsNew[0].donGia}</Text>
               </View>
-              
+              <View>
+
+                <Text style={{fontSize:25, fontWeight:'bold'}}>
+                    Description
+                </Text>
+                <Text>
+                    It is a very important form of writing as we write almost verything paragraphs, be it an answer, essay, storry, emails, etc. 
+                </Text>
+              </View>
               <View style={{width:'94%', marginLeft:'3%', marginTop:'20%'}}><Button title='Add to card' ></Button></View>
            </View>
         </View>
@@ -21,12 +28,10 @@ export default function Screen_03 ({route}) {
 
 const styles = StyleSheet.create({
     container: {
-       
-        flex:1
+       flex:2
     },
-    conten1: {
-        
-       
+    conten1: { 
+        flex:1,
         height:500
        
     },
